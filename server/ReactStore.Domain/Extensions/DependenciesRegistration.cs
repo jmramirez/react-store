04ca76412d8ff2853b1aa30ff4a2b046aac379dd
Jose Ramirez <josemramirez81@outlook.com>
2021-06-07 17:54:36 -0400

    Register product dependencies

---
+++ server/ReactStore.Domain/Extensions/DependenciesRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ReactStore.Domain.Mappers;
+using ReactStore.Domain.Services;
+
+namespace ReactStore.Domain.Extensions
+{
+    public static class DependenciesRegistration
+    {
+        public static IServiceCollection AddMappers(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IProductMappers, ProductMappers>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services
+                .AddScoped<IProductServices, ProductServices>();
+
+            return services;
+        }
+    }
+}
