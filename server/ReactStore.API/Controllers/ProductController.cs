88792a6e7027d609b0e6a1de98686d84f78737db
Jose Ramirez <josemramirez81@outlook.com>
2021-06-07 17:55:28 -0400

    Define HttpGet request method to get list of products

---
+++ server/ReactStore.API/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactStore.Domain.Services;
+
+namespace ReactStore.API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]   
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductServices _productServices;
+
+        public ProductController(IProductServices productServices)
+        {
+            _productServices = productServices;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string q, string brands, int? minPrice, int? maxPrice, int? minScreen,
+            int? maxScreen, string capacity, string colors, string os, string features)
+        {
+            var result = await _productServices.GetProductsAsync( q, brands, minPrice, maxPrice, minScreen, maxScreen, capacity, colors, os, features);
+            return Ok(result);
+        }
+    }
+}
