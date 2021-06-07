301a4bf4b2f7c8a28498cc865677481cd3e3dfbf
Jose Ramirez <josemramirez81@outlook.com>
2021-06-07 17:01:42 -0400

    Define Product for list response item

---
+++ server/ReactStore.Domain/Responses/ProductForListResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReactStore.Domain.Responses
+{
+    public class ProductForListResponse
+    {
+        public Guid Id { get; set; }
+        public string Slug { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public decimal Price { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
