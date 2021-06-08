593f36757463dcf70f25da80aac08b97c36cd771
Jose Ramirez <josemramirez81@outlook.com>
2021-06-08 16:09:14 -0400

    Add styling to Header

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import './App.scss';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import {CataloguePage} from './pages/CataloguePage/CataloguePage'
 import {ProductPage} from './pages/ProductPage/ProductPage'
+import {Header} from './components/Header/Header'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route path='/' component={CataloguePage} />
           <Route path='/products' component={ProductPage} />
