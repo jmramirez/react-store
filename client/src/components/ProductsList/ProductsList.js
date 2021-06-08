7a06da0345d8791ae5f4d47e34cc79a974cc2481
Jose Ramirez <josemramirez81@outlook.com>
2021-06-08 18:08:06 -0400

    Setup method to fetch data from backend

--- client/src/components/ProductsList/ProductsList.js
+++ client/src/components/ProductsList/ProductsList.js
@@ -1,3 +1,33 @@
 import './ProductsList.scss'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
-export const ProductsList = () => <h1>Products List</h1>
+export const ProductsList = () => {
+  
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get('https://localhost:5001/api/products')
+        setProducts(response.data)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    getProducts()
+  }, [])
+
+  return(
+    <>
+      <h1>Product Page</h1>
+      <ul>
+        { products.map((product) => (
+          <li key={product.id}>
+            {product.name}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
