3035626c56fcc72a064038b5c91db702bc449deb
Jose Ramirez <josemramirez81@outlook.com>
2021-06-08 15:56:04 -0400

    Create Header component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.scss'
+import {Link} from 'react-router-dom'
+
+export const Header = () => (
+  <header className="header">
+    <nav className="header-nav">
+      <Link to="/" className="header-nav__heading">
+        ReactCoreStore
+      </Link>
+    </nav>
+  </header>
+)
