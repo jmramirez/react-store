dcab7e14a8c76f18865be7dd364fd964c6959f39
Jose Ramirez <josemramirez81@outlook.com>
2021-06-12 13:31:51 -0400

    Create FilterItem component

---
+++ client/src/components/FilterItem/FilterItem.js
@@ -0,0 +1,10 @@
+﻿import './FilterItem.scss'
+
+export const FilterItem = ({ title }) => (
+    <div className="filter">
+        <div className="filter-header">
+            <h3 className="filter-header__name">{title}</h3>
+            <span className="material-icons">expand_less</span>
+        </div>
+    </div>
+)
--- client/src/components/FiltersList/FiltersList.js
+++ client/src/components/FiltersList/FiltersList.js
@@ -1,7 +1,14 @@
 ﻿import './FiltersList.scss'
+import {FilterItem} from "../FilterItem/FilterItem";
 
 export const FiltersList = () => (
     <div className="filters-list">
-        <h3>Filters List</h3>
+        <FilterItem title="Brands"/>
+        <FilterItem title="Price"/>
+        <FilterItem title="Screen Size"/>
+        <FilterItem title="Color"/>
+        <FilterItem title="Operating System"/>
+        <FilterItem title="Features"/>
+        <FilterItem title="Capacity"/>
     </div>
 )
