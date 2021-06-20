4a8e9a2f210663c417139f67404acbb167bbef41
Jose Ramirez <josemramirez81@outlook.com>
2021-06-20 00:01:16 -0400

    Create FilterElement for individual products spec

---
+++ client/src/components/FilterElement/FilterElement.js
@@ -0,0 +1,13 @@
+﻿import './FilterElement.scss'
+import {useState} from "react";
+
+export const FilterElement = ({ name, setQuery, active }) => {
+    return(
+        <div 
+            className={`filter-item ${active? 'filter-item--active' : ''}`} 
+            onClick={() => setQuery(name)}
+        >
+            <h3 className="filter-item__header">{name}</h3>
+        </div>
+    )
+}
