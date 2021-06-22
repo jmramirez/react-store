39e4e8bfc149222824f0cd7359aa857ac1e8b720
Jose Ramirez <josemramirez81@outlook.com>
2021-06-22 13:21:02 -0400

    Define ProductDetails DTO object

--- server/ReactStore.Domain/Mappers/ProductMappers.cs
+++ server/ReactStore.Domain/Mappers/ProductMappers.cs
@@ -7,6 +7,7 @@ namespace ReactStore.Domain.Mappers
     public interface IProductMappers
     {
         ProductForListResponse Map(Product request);
+        ProductItemResponse MapProduct(Product request);
     }
     
     public class ProductMappers : IProductMappers
@@ -25,6 +26,37 @@ namespace ReactStore.Domain.Mappers
                 Price = request.ProductVariants.OrderBy(v => v.Price).First().Price
             };
 
+            return response;
+        }
+
+        public ProductItemResponse MapProduct(Product request)
+        {
+            if (request == null) return null;
+
+            var response = new ProductItemResponse
+            {
+                Id = request.Id,
+                Slug = request.Slug,
+                Name = request.Name,
+                ShortDescription = request.ShortDescription,
+                Description = request.Description,
+                Thumbnail = request.Thumbnail,
+                Price = request.ProductVariants.OrderBy(v => v.Price).First().Price,
+                Images = request.Images.Select(i => new ImageResponse{ Id = i.Id, Url = i.Url }),
+                Features = request.ProductFeatures.Select(pf => new FeatureResponse{ Id = pf.FeatureId, Name = pf.Feature.Name}),
+                ProductVariants = request.ProductVariants.Select(pv => new VariantsResponse
+                {
+                    ProductId = request.Id,
+                    Name = request.Name,
+                    Thumbnail = request.Thumbnail,
+                    ColorId = pv.ColorId,
+                    Color = pv.Color.Name,
+                    StorageId = pv.StorageId,
+                    Capacity = pv.Storage.Capacity,
+                    Price = pv.Price
+                })
+            };
+            
             return response;
         }
     }
