b4f79f1ad9e8b7f0d88c42581fbec1b109b8f9e0
Jose Ramirez <josemramirez81@outlook.com>
2021-06-29 12:06:51 -0400

    Create ProductImage component to show the main image of products

---
+++ client/src/components/ProductImage/ProductImage.js
@@ -0,0 +1,37 @@
+﻿import './ProductImage.scss'
+import {ProductImageThumb} from '../ProductImageThumb/ProductImageThumb'
+import {useEffect, useState} from 'react'
+
+export const ProductImage = ({ images }) => {
+  const [imagesList, setImagesList] = useState(images)
+  const [image, setImage] = useState(images[0])
+  const [imageId, setImageId] = useState('')
+  
+  useEffect(() => {
+    const filteredImage = images.filter(imageSelected => imageSelected.id !== image.id)
+    setImagesList(filteredImage)
+  }, [imageId])
+  
+  const changeImage = (imageId) => {
+    const newImage = images.find(image => image.id === imageId)
+    setImage(newImage)
+    setImageId(imageId)
+  }
+  
+  return (
+    <div className="image-container">
+      <div className="image-container-main">
+        <img src={image.url} alt="product_img" className="image-container-main__img"/>
+      </div>
+      <div className="image-container-thumbs">
+        <ul className="image-container-thumbs__list">
+          {imagesList.map((image) => (
+            <li key={image.id}>
+              <ProductImageThumb image={image} changeImage={changeImage}/>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
