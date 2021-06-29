fd6ddc084b0faa49f171f35849372f2d1055c098
Jose Ramirez <josemramirez81@outlook.com>
2021-06-29 12:04:25 -0400

    Create ImageThumb container to show images thumbnails

---
+++ client/src/components/ProductImageThumb/ProductImageThumb.js
@@ -0,0 +1,7 @@
+﻿import './ProductImageThumb.scss'
+
+export  const ProductImageThumb = ({ image, changeImage }) => (
+  <div className="thumbnail-container" onClick={() => changeImage(image.id)}>
+    <img src={image.url} alt="prod_img" className="thumbnail-container__image" />
+  </div>
+)
