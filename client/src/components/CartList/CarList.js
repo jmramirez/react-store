926125937b2b2f2c0baa5f5294ad0d7d9f436dde
Jose Ramirez <josemramirez81@outlook.com>
2021-07-03 11:44:32 -0400

    Create CartList component

---
+++ client/src/components/CartList/CarList.js
@@ -0,0 +1,33 @@
+﻿import './CarList.scss'
+import { useSelector } from 'react-redux'
+import {CartItem} from '../CartItem/CartItem'
+import {useEffect, useState} from 'react'
+
+export const CarList = () => {
+  const items = useSelector((state) => state.cart.cartItems)
+  const [subtotal, setSubtotal] = useState(0)
+
+  useEffect(() => {
+    const reducer = (accumulator, cartItem) => accumulator + cartItem.subTotal;
+    const total = items.reduce(reducer,0)
+    setSubtotal(total)
+  },[items])
+  
+  return (
+    <ul>
+      <li className="cartList__header">
+        <div className="cartList__header-product"><p className="cartList__header__labels">Products</p></div>
+        <div className="cartList__header-product-info"><p className="cartList__header__labels">Price</p></div>
+        <div className="cartList__header-product-info"><p className="cartList__header__labels">Quatity</p></div>
+        <div className="cartList__header-product-info"><p className="cartList__header__labels">Subtotal</p></div>
+        <div className="cartList__header-product-action">
+
+        </div>
+      </li>
+      {items.map(item => 
+        <li key={item.productId + item.colorId + item.storageId} className="cartList__body">
+          <CartItem item={item} />
+        </li>)}
+    </ul>
+  )
+}
