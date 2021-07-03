0503ec198966690073b6a973cd81cc8e3ad6ae80
Jose Ramirez <josemramirez81@outlook.com>
2021-07-03 11:43:12 -0400

    Create cart page

---
+++ client/src/pages/CartPage/CartPage.js
@@ -0,0 +1,13 @@
+﻿import './CartPage.scss'
+import {CarList} from '../../components/CartList/CarList'
+
+export  const CartPage = (history) => {
+  return(
+    <section className="shoppingCart">
+      <h2 className="shoppingCart__heading">ShoppingCart</h2>
+      <div className="shoppingCart__container">
+        <CarList />
+      </div>
+    </section>
+  )
+}
