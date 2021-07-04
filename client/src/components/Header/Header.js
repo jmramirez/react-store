5bd76403403df86c1c1654b2d532086e09af342e
Jose Ramirez <josemramirez81@outlook.com>
2021-07-04 17:16:23 -0400

    Add ShoppingCart Link with number of items.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,30 @@
 import './Header.scss'
 import {Link} from 'react-router-dom'
+import { useSelector} from 'react-redux'
+import {useEffect, useState} from 'react'
 
-export const Header = () => (
-  <header className="header">
-    <nav className="header-nav">
-      <Link to="/" className="header-nav__heading">
-        CoreNETStore
-      </Link>
-    </nav>
-  </header>
-)
+export const Header = () => {
+  const items = useSelector((state) => state.cart.cartItems)
+  const [totalItems, setTotalItems] = useState(0)
+  
+  useEffect(() => {
+    const reducer = (accumulator, cartItem) => accumulator + cartItem.quantity;
+    const totalItems = items.reduce(reducer, 0)
+    setTotalItems(totalItems)
+  }, [items])
+  
+  return (
+    <header className="header">
+      <nav className="header-nav">
+        <Link to="/" className="header-nav__heading">
+          CoreNETStore
+        </Link>
+        <Link to="/cart" className="header-nav__shoppingCart">
+          <span className="material-icons header-nav__shoppingCart__icon">shopping_cart</span>
+          {totalItems ? `(${totalItems} Items)` : ''} 
+        </Link>
+        
+      </nav>
+    </header>
+  )
+}
