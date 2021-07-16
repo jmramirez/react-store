04668ccccda43ac25911cb9fa88d856d07e79ffe
Jose Ramirez <josemramirez81@outlook.com>
2021-07-15 23:21:57 -0400

    Implement UserRepository

---
+++ server/ReactStore.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ReactStore.Domain.Entities;
+
+namespace ReactStore.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<bool> AuthenticateAsync(string email, string password, CancellationToken cancellationToken = default);
+        Task<bool> SignUpAsync(AppUser user, string password, CancellationToken cancellationToken = default);
+        Task<AppUser> GetByEmailAsync(string requestEmail, CancellationToken cancellationToken = default);
+        Task<IEnumerable<string>> GetUserRoles(string requestEmail, CancellationToken cancellationToken = default);
+    }
+}
---
+++ server/ReactStore.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ReactStore.Domain.Entities;
+using ReactStore.Domain.Repositories;
+
+namespace ReactStore.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public UserRepository(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, 
+            RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<bool> AuthenticateAsync(string email, string password, CancellationToken cancellationToken)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, false, false);
+            return result.Succeeded;
+        }
+
+        public async Task<bool> SignUpAsync(AppUser user, string password, CancellationToken cancellationToken)
+        {
+            var result = await _userManager.CreateAsync(user, password);
+            return result.Succeeded;
+        }
+
+        public async Task<AppUser> GetByEmailAsync(string requestEmail, CancellationToken cancellationToken)
+        {
+            return await _userManager
+                .Users
+                .FirstOrDefaultAsync(u => u.Email == requestEmail, cancellationToken);
+        }
+
+        public async Task<IEnumerable<string>> GetUserRoles(string requestEmail,
+            CancellationToken cancellationToken = default)
+        {
+            var user = await _userManager.Users
+                .FirstOrDefaultAsync(u => u.Email == requestEmail, cancellationToken);
+
+            return await _userManager.GetRolesAsync(user);
+        }
+    }
+}
