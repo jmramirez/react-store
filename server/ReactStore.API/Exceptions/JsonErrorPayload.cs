a89143b3a88149b8d1f5f819244b5571ccbc0fae
Jose Ramirez <josemramirez81@outlook.com>
2021-07-15 23:24:45 -0400

    Define method to generate custom json response

---
+++ server/ReactStore.API/Exceptions/JsonErrorPayload.cs
@@ -0,0 +1,8 @@
+﻿namespace ReactStore.API.Exceptions
+{
+    public class JsonErrorPayload
+    {
+        public int EventId { get; set; }
+        public object DetailedMessage { get; set; }
+    }
+}
