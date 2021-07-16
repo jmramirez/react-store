045e1c299e71b7132975eaa4ce668eb8b5f49823
Jose Ramirez <josemramirez81@outlook.com>
2021-07-15 23:25:27 -0400

    Create DatabaseSchema to insert new data

---
+++ server/ReactStore.Infrastructure/SchemaDefinitions/RolesSchemaDefinition.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReactStore.Domain.Entities;
+
+namespace ReactStore.Infrastructure.SchemaDefinitions
+{
+    public class RolesSchemaDefinition : IEntityTypeConfiguration<AppRole>
+    {
+        public void Configure(EntityTypeBuilder<AppRole> builder)
+        {
+            builder.HasData(
+                new AppRole
+                {
+                    Id = 1,
+                    Name = "Admin"
+                },
+                new AppRole
+                {
+                    Id = 2,
+                    Name = "Customer"
+                }
+            );
+            
+        }
+    }
+}
