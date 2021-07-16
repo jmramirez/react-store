1ca02536ca20b4ec4938f272bdeccb0e2a6cc7c5
Jose Ramirez <josemramirez81@outlook.com>
2021-07-15 23:19:58 -0400

    Seeding User/Roles data to database

---
+++ server/ReactStore.API/Migrations/20210714045700_AddRoles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReactStore.API.Migrations
+{
+    public partial class AddRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { 1, "50b92a8e-9237-4c07-971a-711046eabd3b", "Admin", null });
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ server/ReactStore.API/Migrations/20210714050356_AddAllRoles.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReactStore.API.Migrations
+{
+    public partial class AddAllRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "84fd78a0-d89d-4912-bab9-f831763c19fb");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { 2, "4b0e67f1-b8cd-4790-a374-82ea08f1cdde", "Customer", null });
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "50b92a8e-9237-4c07-971a-711046eabd3b");
+
+            
+        }
+    }
+}
