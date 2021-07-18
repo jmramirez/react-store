932903b0837dae01aa9f1064ef1ae926cda2cd5f
Jose Ramirez <josemramirez81@outlook.com>
2021-07-17 23:54:21 -0400

    Include Product Total info to CartList component

--- client/src/components/CartList/CarList.js
+++ client/src/components/CartList/CarList.js
@@ -2,6 +2,7 @@
 import { useSelector } from 'react-redux'
 import {CartItem} from '../CartItem/CartItem'
 import {useEffect, useState} from 'react'
+import {Link} from "react-router-dom";
 
 export const CarList = () => {
   const items = useSelector((state) => state.cart.cartItems)
@@ -14,20 +15,52 @@ export const CarList = () => {
   },[items])
   
   return (
-    <ul>
-      <li className="cartList__header">
-        <div className="cartList__header-product"><p className="cartList__header__labels">Products</p></div>
-        <div className="cartList__header-product-info"><p className="cartList__header__labels">Price</p></div>
-        <div className="cartList__header-product-info"><p className="cartList__header__labels">Quatity</p></div>
-        <div className="cartList__header-product-info"><p className="cartList__header__labels">Subtotal</p></div>
-        <div className="cartList__header-product-action">
+      <div>
+      {items.length ?
+          (
+              <ul className="cartList">
+                  <li className="cartList__header">
+                      <div className="cartList__header-product"><p className="cartList__header__labels">Products</p>
+                      </div>
+                      <div className="cartList__header-product-info"><p
+                          className="cartList__header__labels">Price</p></div>
+                      <div className="cartList__header-product-info"><p
+                          className="cartList__header__labels">Quatity</p></div>
+                      <div className="cartList__header-product-info"><p
+                          className="cartList__header__labels">Subtotal</p></div>
+                      <div className="cartList__header-product-action">
 
-        </div>
-      </li>
-      {items.map(item => 
-        <li key={item.productId + item.colorId + item.storageId} className="cartList__body">
-          <CartItem item={item} />
-        </li>)}
-    </ul>
+                      </div>
+                  </li>
+                  {items.map(item =>
+                      <li key={item.productId + item.colorId + item.storageId} className="cartList__body">
+                          <CartItem item={item}/>
+                      </li>)
+                  }
+              </ul>
+          )
+          : (<p className="cartList__empty">You don't have anything in your cart yet!</p>)
+      }    
+      <div className="cartList-actions">
+          <Link to='/' className="cartList-actions__link-back">
+              <span className="material-icons cartList-actions__link-back__icon">arrow_back</span>
+              <span className="cartList-actions__link-back__text">Continue Shopping</span>
+          </Link>
+          {
+              items.length > 0 && (
+                  <>
+                      <div className="cartList-actions__total">
+                          <p className="cartList-actions__total__content">Product Total: ${subtotal}</p>
+                      </div>
+                      <Link to='/' className="cartList-actions__link-check">
+                      <span className="cartList-actions__link-check__text">Chekout</span>
+                      <span className="material-icons cartList-actions__link-check__icon">arrow_forward</span>    
+                      </Link>
+                  </>
+              )
+          }
+      </div>
+      </div>  
+      
   )
 }
