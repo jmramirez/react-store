464faee695d2fc23a386ac0bdba8646dfede90aa
Jose Ramirez <josemramirez81@outlook.com>
2021-08-19 13:57:39 -0400

    Refactor Modal component to include LogIn/Register Form

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,15 +1,15 @@
 ﻿import './Modal.scss'
+import LoginForm from "../LogInForm/LogInForm";
 
 const Modal = ({ modalOpen, handleOpenModal }) => {
     return(
         <div className={ modalOpen ? 'modal--open' : 'modal'}>
             <div className="modal-background"></div>
             <div className="modal-body">
-                <div className="modal-content">
-                    <button className="modal__close-button" onClick={handleOpenModal}><span className="modal__close-icon"></span></button>
-                    <h1 className="modal-content__header"> This is a modal</h1>
-                </div>
+                <button className="modal__close-button" onClick={handleOpenModal}><span className="modal__close-icon"></span></button>
+                <LoginForm/>
             </div>
+            
         </div>
     )
 }
