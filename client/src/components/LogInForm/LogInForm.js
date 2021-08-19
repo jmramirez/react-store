635c9ec34e124c4f3380832fcb48347efb98baa6
Jose Ramirez <josemramirez81@outlook.com>
2021-08-19 13:58:38 -0400

    Create LogInForm component

---
+++ client/src/components/LogInForm/LogInForm.js
@@ -0,0 +1,30 @@
+﻿import './LogInForm.scss'
+
+const LoginForm = () => {
+    return(
+        <form className="form">
+            <div className="form-content">
+                <h1 className="form-content__header">Log In</h1>
+                <label className="form-content__label">
+                    Username/Email
+                </label>
+                <input className="form-content__input"/>
+                <label className="form-content__label">
+                    Password
+                </label>
+                <input type="password" className="form-content__input"/>
+                <button className="form-content__button">Not account? Create one!</button>
+            </div>
+            <div className="form-action">
+                <button>
+                    Cancel
+                </button>
+                <button>
+                    Sign In
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm
