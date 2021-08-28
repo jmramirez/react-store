a18290c0f68069afa554a417b2d5847875494a52
Jose Ramirez <josemramirez81@outlook.com>
2021-08-28 15:19:29 -0400

    Modify OderItem attribute name

--- server/ReactStore.Domain/Entities/OrderItem.cs
+++ server/ReactStore.Domain/Entities/OrderItem.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int OrderId { get; set; }
-        public int ProjectId { get; set; }
+        public int ProductId { get; set; }
         public int ColorId { get; set; }
         public int StorageId { get; set; }
         public int Quantity { get; set; }
