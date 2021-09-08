291754b04b17a5dcb1076f46a816b4027a4469e0
Jose Ramirez <josemramirez81@outlook.com>
2021-09-08 00:55:36 -0400

    Create PrivateRoute component to redirect not authenticated users

---
+++ client/src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,21 @@
+﻿import { useSelector } from "react-redux";
+import { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+    
+    return(
+        <Route {...rest} render={props =>(
+          userInfo?(
+              <Component {...props} />
+          ) : (
+              <Redirect to={{
+                  pathname: '/cart',
+                  state: { from: props.location}
+              }}/>
+          )  
+        )}/>
+    )
+}
