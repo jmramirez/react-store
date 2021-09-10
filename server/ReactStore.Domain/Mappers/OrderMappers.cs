d69cd88b24e2c10dec476fcea52c363ac6fc7e36
Jose Ramirez <josemramirez81@outlook.com>
2021-09-10 13:20:42 -0400

    Define Order mappers methods

---
+++ server/ReactStore.Domain/Mappers/OrderMappers.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using ReactStore.Domain.Entities;
+using ReactStore.Domain.Requests.Order;
+using ReactStore.Domain.Responses;
+
+namespace ReactStore.Domain.Mappers
+{
+    public interface IOrderMappers
+    {
+        Order Map(CreateOrderRequest request);
+        OrderItem Map(OrderItemRequest request);
+        OrderCreatedResponse Map(Order order, string paymentStatus);
+    }
+    
+    public class OrderMappers : IOrderMappers
+    {
+        public Order Map(CreateOrderRequest request)
+        {
+            if (request == null) return null;
+
+            var order = new Order
+            {
+                DeliveryAddress = new Address
+                {
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                    Address1 = request.Address1,
+                    Address2 = request.Address2,
+                    TownCity = request.TownCity,
+                    Country = request.Country,
+                    Postcode = request.Postcode
+                },
+                Items = request.Items.Select(x => Map(x)).ToList()
+            };
+
+            return order;
+        }
+
+        public OrderItem Map(OrderItemRequest request)
+        {
+            if (request == null) return null;
+
+            var orderItem = new OrderItem
+            {
+                ProductId = request.ProductId,
+                ColorId = request.ColorId,
+                StorageId = request.StorageId,
+                Quantity = request.Quantity
+            };
+
+            return orderItem;
+        }
+
+        public OrderCreatedResponse Map(Order order, string paymentStatus)
+        {
+            if (order == null) return null;
+
+            var response = new OrderCreatedResponse
+            {
+                OrderId = order.Id,
+                PaymentStatus = paymentStatus
+            };
+
+            return response;
+        }
+    }
+}
