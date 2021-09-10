d4ceef63fdd2aa6af1f0bd89a26806adbea6760f
Jose Ramirez <josemramirez81@outlook.com>
2021-09-10 13:15:47 -0400

    Define OrderRepository interface with methods to add order to database

---
+++ server/ReactStore.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ReactStore.Domain.Entities;
+
+namespace ReactStore.Domain.Repositories
+{
+    public interface IOrderRepository : IRepository
+    {
+        Task<IEnumerable<Order>> GetAsync();
+        Task<int> GetTotal(int id);
+        Order Update(Order order);
+    }
+}
