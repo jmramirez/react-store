00f188803f38bc668da1526d30fb9e2a6fa5c28d
Jose Ramirez <josemramirez81@outlook.com>
2021-09-10 13:14:16 -0400

    Define database schema elements for Order/OrderItems

---
+++ server/ReactStore.Infrastructure/SchemaDefinitions/OrderItemSchemaDefinition.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReactStore.Domain.Entities;
+
+namespace ReactStore.Infrastructure.SchemaDefinitions
+{
+    public class OrderItemSchemaDefinition : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItems", ReactStoreContext.DEFAULT_SCHEMA);
+            builder.HasKey(k => k.Id);
+
+            builder
+                .HasOne(v => v.ProductVariant)
+                .WithMany(pv => pv.Items)
+                .HasForeignKey(fk => new { fk.ColorId, fk.ProductId, fk.StorageId});
+        }
+    }
+}
---
+++ server/ReactStore.Infrastructure/SchemaDefinitions/OrderSchemaDefinition.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReactStore.Domain.Entities;
+
+namespace ReactStore.Infrastructure.SchemaDefinitions
+{
+    public class OrderSchemaDefinition : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders", ReactStoreContext.DEFAULT_SCHEMA);
+            builder.HasKey(k => k.Id);
+
+            builder
+                .HasOne(u => u.User)
+                .WithMany(o => o.Orders)
+                .HasForeignKey(fk => fk.UserId);
+            
+            
+            
+            builder
+                .OwnsOne(x => x.DeliveryAddress);
+        }
+    }
+}
