3343280bd604840028b603ed1493fadebd0fc3ff
Jose Ramirez <josemramirez81@outlook.com>
2021-09-10 12:34:11 -0400

    Include useeffect hook so the page redirects when user is not authenticated

--- client/src/pages/CheckoutPage/CheckoutPage.js
+++ client/src/pages/CheckoutPage/CheckoutPage.js
@@ -2,9 +2,25 @@
 import CheckoutForm from "../../components/CheckoutForm/CheckoutForm";
 import {loadStripe} from "@stripe/stripe-js";
 import {Elements} from "@stripe/react-stripe-js";
+import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+import {Redirect} from "react-router-dom";
 
 export const CheckoutPage = () => {
     const stripe = loadStripe('pk_test_51JSqRkHyqUWXkZvhkJrM0cWerW3BjJ4IBhbcKqa4Sz7CoT3HouLB6EPCAGbFg3cvtRkpDT5NhksuZHfQepcnXKIo00U2xnWNp7')
+    const items = useSelector((state) => state.cart.cartItems)
+    const [redirectToCart, setRedirectToCart] = useState(false) 
+    
+    
+    useEffect(() =>{
+        if(!items.length) {
+            setRedirectToCart(true)
+        }
+    }, [])
+    
+    if(redirectToCart) {
+        return <Redirect to='/cart' />
+    }
     
     return(
         <section className="checkout">
