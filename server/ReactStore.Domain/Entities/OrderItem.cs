2f75cf635bdd1b3a1456be994c6c98b6ed3cb620
Jose Ramirez <josemramirez81@outlook.com>
2021-09-10 13:04:04 -0400

    Define OrderItem entity class

--- server/ReactStore.Domain/Entities/OrderItem.cs
+++ server/ReactStore.Domain/Entities/OrderItem.cs
@@ -1,12 +1,14 @@
-﻿namespace ReactStore.Domain.Entities
+﻿using System;
+
+namespace ReactStore.Domain.Entities
 {
     public class OrderItem
     {
         public int Id { get; set; }
         public int OrderId { get; set; }
-        public int ProductId { get; set; }
-        public int ColorId { get; set; }
-        public int StorageId { get; set; }
+        public Guid ProductId { get; set; }
+        public Guid ColorId { get; set; }
+        public Guid StorageId { get; set; }
         public int Quantity { get; set; }
         
         public ProductVariant ProductVariant { get; set; }
