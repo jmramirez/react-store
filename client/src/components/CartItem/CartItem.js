f2c6081710d0b01a1e191ae1b2a81b9662bedc83
Jose Ramirez <josemramirez81@outlook.com>
2021-10-17 19:25:54 -0400

    Refactor styling so the Shopping Cart page is fully responsive

--- client/src/components/CartItem/CartItem.js
+++ client/src/components/CartItem/CartItem.js
@@ -21,47 +21,54 @@ export const CartItem = ({ item }) => {
 
   return(
     <div className="cartItem">
-      <div className="cartItem-product">
-        <Link to={`/products/${item.slug}`} className="cartItem-product__image">
-          <img src={item.thumbnail} alt="prod_img" className="cartItem-product__image__thumb" />
-        </Link>
-        <div className="cartItem-product__details">
-          <p className="cartItem-product__details__label__name">{item.name}</p>
-          <p className="cartItem-product__details__label">
-            <span className="cartItem-product__details__label__name">Color:</span> {item.color}
-          </p>
-          <p className="cartItem-product__details__label">
-            <span className="cartItem-product__details__label__name">Capacity: </span> {item.capacity}
-          </p>
+      <div className="cartItem__info">
+        <div className="cartItem__info-product">
+          <Link to={`/products/${item.slug}`} className="cartItem__info-product__image">
+            <img src={item.thumbnail} alt="prod_img" className="cartItem__info-product__image__thumb" />
+          </Link>
+          <div className="cartItem__info-product__details">
+            <p className="cartItem__info-product__details__label__name">{item.name}</p>
+            <p className="cartItem__info-product__details__label">
+              <span className="cartItem__info-product__details__label__name">Color:</span> {item.color}
+            </p>
+            <p className="cartItem__info-product__details__label">
+              <span className="cartItem__info-product__details__label__name">Capacity: </span> {item.capacity}
+            </p>
+          </div>
+        </div>
+        <div className="cartItem__info-price">
+          <p className="cartItem__info-price__heading">Price: </p>
+          <p className="cartItem__info-price__label">${item.price}</p>
+        </div>
+        <div className="cartItem__info-quantity">
+          <p className="cartItem__info-quantity__heading">Quantity: </p>
+          <div className="cartItem__info-quantity__top">
+            <button disabled={item.quantity < 2} className="cartItem__info-quantity__button" onClick={() => removeProduct(item)}>
+              <span className="material-icons cartItem__info-quantity__button__span">remove</span>
+            </button>
+            <p className="cartItem__info-quantity__label">{item.quantity}</p>
+            <button disabled={item.quantity > 9}  className="cartItem__info-quantity__button" onClick={() => addProduct(item)}>
+              <span className="material-icons cartItem__info-quantity__button__span">add</span>
+            </button>
+          </div>
+          {
+            item.quantity > 9 &&
+            (
+              <div className="cartItem__info-quantity__bottom">
+                10 items max.
+              </div>
+            )
+          }
+        </div>
+        <div className="cartItem__info-subtotal">
+          <p className="cartItem__info-price__heading">Subtotal: </p>
+          <p className="cartItem__info-price__label"> ${item.subTotal}</p>
         </div>
       </div>
-      <div className="cartItem-price">
-        <p className="cartItem-price__label">${item.price}</p>
-      </div>
-      <div className="cartItem-quantity">
-        <div className="cartItem-quantity__top">
-          <button disabled={item.quantity < 2} className="cartItem-quantity__button" onClick={() => removeProduct(item)}>
-            <span className="material-icons cartItem-quantity__button__span">remove</span>
-          </button>
-          <p className="cartItem-quantity__label">{item.quantity}</p>
-          <button disabled={item.quantity > 9}  className="cartItem-quantity__button" onClick={() => addProduct(item)}>
-            <span className="material-icons cartItem-quantity__button__span">add</span>
-          </button>
-        </div>
-        {
-          item.quantity > 9 && 
-          (
-            <div className="cartItem-quantity__bottom">
-              10 items max.
-            </div>      
-          )
-        }
-      </div>
-      <div className="cartItem-subtotal">
-        <p className="cartItem-price__label"> ${item.subTotal}</p></div>
       <div className="cartItem-action">
         <button className="cartItem-action__button" onClick={() => handleClick(item)}>
-          <span className="material-icons cartItem-action__button__span">delete_forever</span> Remove Item
+          <span className="material-icons cartItem-action__button__span">delete_forever</span>
+          <p className="cartItem-action__button__text">Remove Item</p>
         </button>
       </div>
     </div>
