23d9b7318e8e29cfccceb8b54f0f3bb4ab53dcf3
Jose Ramirez <josemramirez81@outlook.com>
2021-10-17 01:54:03 -0400

    Refactor code for responsiveness

--- client/src/components/Filters/Filters.js
+++ client/src/components/Filters/Filters.js
@@ -3,7 +3,7 @@ import {FiltersList} from "../FiltersList/FiltersList";
 
 export const Filters = ({ location, history}) => (
     <div className="filters">
-        <h2 className="filters__heading">Filters</h2>
+        <div className="filters__header"><h2 className="filters__header__heading">Filters</h2><span className={`material-icons filters__header__icon `}>arrow_forward_ios</span></div>
         <FiltersList location={location}  history={history} />
     </div>
 )
