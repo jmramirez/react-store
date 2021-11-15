2b76006ffeb54538aa1e368dac9b8a56500d471f
Jose Ramirez <josemramirez81@outlook.com>
2021-11-14 23:48:58 -0500

    Refactor Seed data

--- server/ReactStore.API/Migrations/20211111044624_initial.cs
+++ server/ReactStore.API/Migrations/20211111044624_initial.cs
@@ -412,8 +412,8 @@ namespace ReactStore.API.Migrations
                 columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
                 values: new object[,]
                 {
-                    { 1, "cc828056-a334-436d-a664-99c8631cd0e7", "Admin", "admin" },
-                    { 2, "89ddd1f9-4b73-44e8-a45d-f813c3c1c6a1", "Customer", "customer" }
+                    { 1, "cc828056-a334-436d-a664-99c8631cd0e7", "Admin", "ADMIN" },
+                    { 2, "89ddd1f9-4b73-44e8-a45d-f813c3c1c6a1", "Customer", "CUSTOMER" }
                 });
 
             migrationBuilder.InsertData(
@@ -436,7 +436,6 @@ namespace ReactStore.API.Migrations
                 values: new object[,]
                 {
                     { new Guid("0ec56401-7552-422d-8c91-e4ae41380fbc"), "Pink" },
-                    { new Guid("fee21a32-ec49-46cd-adf9-8ed4b15211c9"), "Red" },
                     { new Guid("700b2a28-ea84-419c-bf6d-7fe1126b4b4b"), "Red" },
                     { new Guid("ea32ab8f-e614-4327-955e-3b26670cc1c9"), "Purple" },
                     { new Guid("d6ffc78c-7ed0-4bd7-85a0-ac1677d31f59"), "Grey" },
@@ -617,8 +616,8 @@ namespace ReactStore.API.Migrations
                     { new Guid("700b2a28-ea84-419c-bf6d-7fe1126b4b4b"), new Guid("a3478123-d608-4032-b447-b634a2dec89c"), new Guid("80c335a6-eaae-42eb-a83a-0d7f47dc543a"), 1080m },
                     { new Guid("30462f05-b486-4ee0-b2d8-364d5627145d"), new Guid("7e60cf5a-0aab-49d5-9ef2-3f2c9b0f097d"), new Guid("80c335a6-eaae-42eb-a83a-0d7f47dc543a"), 2040m },
                     { new Guid("30462f05-b486-4ee0-b2d8-364d5627145d"), new Guid("533ed09f-6a3a-4ad7-922f-76ebc4a9bc30"), new Guid("80c335a6-eaae-42eb-a83a-0d7f47dc543a"), 2250m },
-                    { new Guid("fee21a32-ec49-46cd-adf9-8ed4b15211c9"), new Guid("8b94cae3-0ef3-4e2e-bbcd-92ec689d3b73"), new Guid("80c335a6-eaae-42eb-a83a-0d7f47dc543a"), 1596m },
-                    { new Guid("fee21a32-ec49-46cd-adf9-8ed4b15211c9"), new Guid("8b94cae3-0ef3-4e2e-bbcd-92ec689d3b73"), new Guid("c1d112fd-5a18-415d-9a5f-9cbc602095a2"), 1740m },
+                    { new Guid("700b2a28-ea84-419c-bf6d-7fe1126b4b4b"), new Guid("8b94cae3-0ef3-4e2e-bbcd-92ec689d3b73"), new Guid("80c335a6-eaae-42eb-a83a-0d7f47dc543a"), 1596m },
+                    { new Guid("700b2a28-ea84-419c-bf6d-7fe1126b4b4b"), new Guid("8b94cae3-0ef3-4e2e-bbcd-92ec689d3b73"), new Guid("c1d112fd-5a18-415d-9a5f-9cbc602095a2"), 1740m },
                     { new Guid("d004ef26-3c08-4f6e-8060-136029b75782"), new Guid("8b94cae3-0ef3-4e2e-bbcd-92ec689d3b73"), new Guid("80c335a6-eaae-42eb-a83a-0d7f47dc543a"), 14596m },
                     { new Guid("d004ef26-3c08-4f6e-8060-136029b75782"), new Guid("8b94cae3-0ef3-4e2e-bbcd-92ec689d3b73"), new Guid("c1d112fd-5a18-415d-9a5f-9cbc602095a2"), 1740m },
                     { new Guid("d004ef26-3c08-4f6e-8060-136029b75782"), new Guid("8b94cae3-0ef3-4e2e-bbcd-92ec689d3b73"), new Guid("52f4cfc5-10a6-41b2-8761-1273de874546"), 2016m },
